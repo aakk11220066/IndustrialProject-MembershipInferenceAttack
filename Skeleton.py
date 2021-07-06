5346f031d7491364e31b4a1352c942749acc7810
shlomil1234 <49244305+shlomil1234@users.noreply.github.com>
2021-07-06 20:24:08 +0300

    Skeleton documentation

--- Skeleton.py
+++ Skeleton.py
@@ -10,12 +10,25 @@ from Trainer import get_regular_model_trainer
 from Models import DisplacementNet
 from Trainer import layer0_weight, layer2_weight, layer0_bias, layer2_bias
 def split_dataset(dataset):
+    """
+    :param dataset:
+    :return splitted data to training set and test set
+    """
     return dataset[:TARGET_TRAIN_DATA_SIZE], dataset[TARGET_TRAIN_DATA_SIZE:]
 
 
 
 def get_attack_model(attack_model_class, target_model, attack_train_features, attack_train_labels,
                      test_features, test_labels):
+    """
+    :param attack_model_class: class of attack model
+    :param target_model: The attacked model
+    :param attack_train_features: attack model training data
+    :param attack_train_labels: attack model training data
+    :param test_features: attack model test data
+    :param test_labels: attack model test data
+    :return trained attack model
+    """
     return attack_model_class(
         target_model=target_model,
         attack_train_features=attack_train_features,
@@ -25,10 +38,18 @@ def get_attack_model(attack_model_class, target_model, attack_train_features, at
 
 
 def experiment(seed: int, attack_model_class):
+    """
+    :param seed:
+    :param attack_model_class: class of attack model
+    :return y_pred: predicted values, y_true: true labels
+    This function making an experiment. At first, build and fit a MLP target model(a model we want to attack). Then, build anf fit a
+    MLP attack model. In the end, testing our attack model.
+    """
     torch.random.manual_seed(seed)
     train_features, train_labels, test_features, test_labels = german_dataset(filename="German.csv")
     train_features = nn.functional.normalize(train_features, dim=0)
     test_features = nn.functional.normalize(test_features, dim=0)
+
     target_train_features, proxy_train_features = split_dataset(train_features)
     target_train_labels, proxy_train_labels = split_dataset(train_labels)
 
@@ -46,16 +67,13 @@ def experiment(seed: int, attack_model_class):
         attack_train_labels=proxy_train_labels,
         test_features=test_features, test_labels=test_labels # DELETEME
     )
-    best_model = torch.load("MyModel")
+    best_model = torch.load("MyModel") # load the best model we saved during the attack model training
     best_model.eval()
 
 
     attack_model.weights_displacer.layer0_conv = best_model.layer0_conv
     attack_model.weights_displacer.layer2_conv = best_model.layer2_conv
-    # attack_model.weights_displacer.layer0_conv.weight = torch.nn.Parameter(layer0_weight)
-    # attack_model.weights_displacer.layer0_conv.bias = torch.nn.Parameter(layer0_bias)
-    # attack_model.weights_displacer.layer2_conv.weight = torch.nn.Parameter(layer2_weight)
-    # attack_model.weights_displacer.layer2_conv.bias = torch.nn.Parameter(layer2_bias)
+
     attack_model
     correct_intrainset_predictions = (
         attack_model(
@@ -77,8 +95,6 @@ def experiment(seed: int, attack_model_class):
                  y=target_train_labels[:test_features.shape[0]]
              )
     )
-    #result = classification_report(y_true, y_pred, labels=None, target_names=None, sample_weight=None, digits=2, output_dict=False, zero_division='warn')
-    # print(result)
 
     print(
         f"Experiment report: \n ",
@@ -91,7 +107,9 @@ def experiment(seed: int, attack_model_class):
 
 
 def main():
-
+    """
+    Making few experiments. Printing classification_report of all experiments.
+    """
     experiment_results = ([experiment(seed, GeneralAttackModel) for seed in SEEDS])
     true_class,predicted_class = [],[]
     for experiment_result in experiment_results:
@@ -103,4 +121,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-#DELETE THIS COMMENT
